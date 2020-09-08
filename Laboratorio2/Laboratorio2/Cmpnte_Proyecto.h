04aa0079d4d787164c70548c32d0d099ef368a81
Manuel Fernández <mgnr7@hotmail.com>
2020-09-08 10:38:45 -0600

    Se agregan metodos a Cmpnte_Proyecto

--- Laboratorio2/Laboratorio2/Cmpnte_Proyecto.h
+++ Laboratorio2/Laboratorio2/Cmpnte_Proyecto.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <string>
+using namespace std;
 
 class Actividad_Hoja;
 
@@ -7,6 +9,18 @@ class Cmpnte_Proyecto
 public:
 
 	virtual Cmpnte_Proyecto* padre() = 0;
+
+	virtual void setPadre(Cmpnte_Proyecto* padre) = 0;
+	virtual Cmpnte_Proyecto getPadre() = 0;
+
+	virtual void agregar(Cmpnte_Proyecto actividad) = 0;
+	virtual void eliminar() = 0;
+
+	virtual void setNombre(string n) = 0;
+	virtual string getNombre() = 0;
+
+	virtual void setResponsable(string r) = 0;
+	virtual string getResponsable() = 0;
 
 
 };
