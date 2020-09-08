f1481cb39163d0163a2015a2b581f0552d61b218
Manuel Fernández <mgnr7@hotmail.com>
2020-09-08 10:48:22 -0600

    Metodo get set descripcion

--- Laboratorio2/Laboratorio2/Cmpnte_Proyecto.h
+++ Laboratorio2/Laboratorio2/Cmpnte_Proyecto.h
@@ -13,7 +13,8 @@ public:
 	virtual void setPadre(Cmpnte_Proyecto* padre) = 0;
 	virtual Cmpnte_Proyecto getPadre() = 0;
 
-	virtual void agregar(Cmpnte_Proyecto actividad) = 0;
+	virtual void agregar(Cmpnte_Proyecto* actividad) = 0;
+	
 	virtual void eliminar() = 0;
 
 	virtual void setNombre(string n) = 0;
@@ -21,6 +22,10 @@ public:
 
 	virtual void setResponsable(string r) = 0;
 	virtual string getResponsable() = 0;
+
+	virtual void setDescripcion(string d) = 0;
+	virtual string getDescripcion() = 0;
+
 
 
 };
